3543bac78ce21a97df39a4306bcff0acc8355c3a
jacobrees <jacob.rees97@yahoo.com>
2021-04-02 13:41:53 +0100

    Create forEach callback method to add books from myLibrary array to html

--- app.js
+++ app.js
@@ -23,7 +23,28 @@ closeFormBtn.addEventListener('click', () => {
   resetForm();
 });
 
-const myLibrary = [];
+const myLibrary = [{
+  title: 'harry potter', author: 'meeeee', pages: '432', read: 'true',
+}, {
+  title: 'harry potter', author: 'meeeee', pages: '432', read: 'true',
+}];
+
+const libraryContainer = document.querySelector('.library-container');
+
+let htmlBooks = '';
+
+myLibrary.forEach((book) => {
+  htmlBooks += `<div class="book-container">
+  <button class="delete-book">DELETE</button>
+  <p class="book-details">Title:<span class="book-details-bold"> ${book.title}</span></p>
+  <p class="book-details">Author:<span class="book-details-bold"> ${book.author}</span></p>
+  <p class="book-details">Pages:<span class="book-details-bold"> ${book.title}</span></p>
+  <p class="book-details">Read:<span class="book-details-bold"> ${book.read}</span></p>
+  <button class="read-book">READ</button>
+</div>`;
+  libraryContainer.innerHTML = htmlBooks;
+});
+
 
 function Book(title, author, pages, read) {
   this.title = title;
