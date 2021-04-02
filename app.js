3d941f3d246e4a4d52c63afe4148ca3418b2b5c6
jacobrees <jacob.rees97@yahoo.com>
2021-04-02 15:29:29 +0100

    Add isbn number to project

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ openFormBtn.addEventListener('click', () => {
 const inputTitle = document.querySelector('#title');
 const inputAuthor = document.querySelector('#author');
 const inputPages = document.querySelector('#pages');
+const inputISBN = document.querySelector('#isbn');
 const inputRead = document.querySelector('#read');
 
 function resetForm() {
   inputTitle.value = '';
   inputAuthor.value = '';
   inputPages.value = '';
+  inputISBN.value = '';
   inputRead.checked = false;
 }
 
@@ -30,10 +32,11 @@ const libraryContainer = document.querySelector('.library-container');
 const submitForm = document.querySelector('.submit-form');
 const alertDanger = document.querySelector('.alert-danger');
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, isbn, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.isbn = isbn;
   this.read = read;
 }
 
@@ -50,6 +53,7 @@ function displayBooks() {
   <p class="book-details">Title:<span class="book-details-bold"> ${book.title}</span></p>
   <p class="book-details">Author:<span class="book-details-bold"> ${book.author}</span></p>
   <p class="book-details">Pages:<span class="book-details-bold"> ${book.pages}</span></p>
+  <p class="book-details">ISBN:<span class="book-details-bold"> ${book.isbn}</span></p>
   <p class="book-details">Read:<span class="book-details-bold"> ${book.read}</span></p>
   <button class="read-book">READ</button>
 </div>`;
@@ -59,14 +63,15 @@ function displayBooks() {
 
 submitForm.addEventListener('click', (e) => {
   e.preventDefault();
-  if (inputTitle.value === '' || inputAuthor.value === '' || inputPages.value === '') {
+  if (inputTitle.value === '' || inputAuthor.value === '' || inputPages.value === '' || inputISBN.value === '') {
     submitForm.disabled = true;
     alertDanger.classList.toggle('hide');
     setTimeout(() => { alertDanger.classList.toggle('hide'); submitForm.disabled = false; }, 3000);
   } else {
-    const book = new Book(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.checked);
+    const book = new Book(inputTitle.value, inputAuthor.value, inputPages.value, inputISBN.value, inputRead.checked); // eslint-disable-line
     addBookToLibrary(book);
     form.classList.toggle('hide');
+    resetForm();
     displayBooks();
   }
 });
