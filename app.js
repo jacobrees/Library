207f84d8110e791dd4fa22ee33b55a7139bf9f46
jacobrees <jacob.rees97@yahoo.com>
2021-04-02 14:40:37 +0100

    Fix functionality to add book to library, add exception handling to form

--- app.js
+++ app.js
@@ -23,28 +23,12 @@ closeFormBtn.addEventListener('click', () => {
   resetForm();
 });
 
-const myLibrary = [{
-  title: 'harry potter', author: 'meeeee', pages: '432', read: 'true',
-}, {
-  title: 'harry potter', author: 'meeeee', pages: '432', read: 'true',
-}];
+const myLibrary = [];
 
 const libraryContainer = document.querySelector('.library-container');
 
-let htmlBooks = '';
-
-myLibrary.forEach((book) => {
-  htmlBooks += `<div class="book-container">
-  <button class="delete-book">DELETE</button>
-  <p class="book-details">Title:<span class="book-details-bold"> ${book.title}</span></p>
-  <p class="book-details">Author:<span class="book-details-bold"> ${book.author}</span></p>
-  <p class="book-details">Pages:<span class="book-details-bold"> ${book.title}</span></p>
-  <p class="book-details">Read:<span class="book-details-bold"> ${book.read}</span></p>
-  <button class="read-book">READ</button>
-</div>`;
-  libraryContainer.innerHTML = htmlBooks;
-});
-
+const submitForm = document.querySelector('.submit-form');
+const alertDanger = document.querySelector('.alert-danger');
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -53,6 +37,38 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+function addBookToLibrary(newBook) {
+  myLibrary.push(newBook);
 }
+
+function displayBooks() {
+  let htmlBooks = '';
+
+  myLibrary.forEach((book) => {
+    htmlBooks += `<div class="book-container">
+  <button class="delete-book">DELETE</button>
+  <p class="book-details">Title:<span class="book-details-bold"> ${book.title}</span></p>
+  <p class="book-details">Author:<span class="book-details-bold"> ${book.author}</span></p>
+  <p class="book-details">Pages:<span class="book-details-bold"> ${book.pages}</span></p>
+  <p class="book-details">Read:<span class="book-details-bold"> ${book.read}</span></p>
+  <button class="read-book">READ</button>
+</div>`;
+  });
+  libraryContainer.innerHTML = htmlBooks;
+}
+
+submitForm.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (inputTitle.value === '' || inputAuthor.value === '' || inputPages.value === '') {
+    submitForm.disabled = true;
+    alertDanger.classList.toggle('hide');
+    setTimeout(() => { alertDanger.classList.toggle('hide'); submitForm.disabled = false; }, 3000);
+  } else {
+    const book = new Book(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.checked);
+    addBookToLibrary(book);
+    form.classList.toggle('hide');
+    displayBooks();
+  }
+});
+
+displayBooks();
